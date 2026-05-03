72a189ca9f92753d73a7182507114bad3c66c3c4
Cameron Stanley <cam96stanley@gmail.com>
2026-05-03 02:58:06 -0400

    fix: fixed build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,6 +23,7 @@ export default function Home() {
               views={bookmark.visitCount}
               lastViewedDate={bookmark.lastVisited}
               addedDate={bookmark.createdAt}
+              isArchived={bookmark.isArchived}
             />
           ))}
       </div>
